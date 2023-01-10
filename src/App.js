f15d6a7cb4b7a8559edce796a74cf54e6b3654c3
Margarita Iglesias <margaritaigle99@gmail.com>
2023-01-09 22:15:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="title">
+
+        <img src="images/escudo.png" className="escudo"/>
+       <h1>Carmelitas Descalzas de Montevideo</h1>
+        </div>
+    
+
+      <nav className="navigate">
+        <li>
+        <a>INICIO</a>
+        </li>
+        <li>
+        <a>¿QUIÉNES SOMOS?</a>
+        </li>
+        <li>
+        <a>CONTACTO</a>
+        </li>
+        <li>
+        <a>OCD FLORIDA</a>
+        </li>
+        
+      </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
